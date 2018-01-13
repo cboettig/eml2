a37da9a6c7b089734e7e3f0afc40f38fe114ab45
Carl Boettiger <cboettig@gmail.com>
2018-01-12 21:04:54 -0800

    a few examples as proof of principle

---
+++ R/eml2.R
@@ -0,0 +1,16 @@
+
+#' read_eml
+#'
+#' read_eml
+#' @param x path to an EML file
+#' @return an eml object (list / S3 object)
+#' @export
+#' @examples
+#' f <- system.file("extdata", "example.xml", package = "emld")
+#' eml <- read_eml(f)
+#' @export
+#' @importFrom emld as_emld
+read_eml <- function(x){
+  emld::as_emld(x)
+}
+
